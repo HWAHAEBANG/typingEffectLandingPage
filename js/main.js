c401f7686c416ca2e76edbf4534b930322113cbb
화해방 <bcl0206@naver.com>
2022-09-06 15:36:28 +0900

    Add question answer

--- js/main.js
+++ js/main.js
@@ -34,3 +34,30 @@ function blink(){
   target.classList.toggle("active");
 }
 setInterval(blink, 500);
+
+/*  출처 jayprogram 블로그
+
+먼저 Element.classList의 반환형은 'DOMTokenList'입니다.
+
+이 DomTokenList의 메서드는 많은 것이 있지만, 오늘은 세가지만 살펴보려고 합니다.
+
+ 
+
+Element.classList.toggle
+
+toggle, 전등 스위치를 껐다 켰다 하는 행위 처럼, 0과 1이 반복되는 행위를 의미합니다.
+즉, 기존 값이 0이었다면 1로 바뀌고 기존 값이 1이었다면, 0으로 바뀌게 됩니다.
+그래서 toggle 메서드는 클래스가 존재한다면 클래스를 제거하고, 클래스가 존재하지 않는다면 클래스를 추가하는 메서드입니다.
+
+ 
+
+Element.classList.add
+
+명시된 클래스를 추가하는 메서드입니다.
+
+ 
+
+Element.classList.remove
+
+명시된 클래스를 제거하는 메서드입니다. */
+
